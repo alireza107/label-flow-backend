53e99694d55a3b54b45fe19603327d7255862d63
Sepideh Shabani <sepidehsh74@gmail.com>
2024-05-04 15:03:17 +0330

    feat category crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectModule } from './project/project.module';
 import { UserModule } from './user/user.module';
 import { dbDataSource } from './data.source';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbDataSource), ProjectModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(dbDataSource),
+    ProjectModule,
+    UserModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
